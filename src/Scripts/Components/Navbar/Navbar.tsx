3298adc4c15c64577e7500f2c95e047005956948
Mulkraj Singh <mulkraj.singh@namshi.com>
2022-08-03 01:12:19 +0530

    project setup

---
+++ src/Scripts/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./Title";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">R R</div>
+      <Title />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Scripts/Components/Navbar/Title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <div className="navbar-name">Read Real</div>;
+};
+
+export default Title;
